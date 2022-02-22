e4bc912e1c7557fd8b104f6b50641d61083f5d50
hsd <51134642@qq.com>
2022-02-22 19:27:49 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import 'virtual:windi-base.css'
+import 'virtual:windi-components.css'
+// 你自定义的 css
+import './styles/main.css'
+
+import 'virtual:windi-utilities.css'
+import 'virtual:windi-devtools'
+
+import App from './App.vue'
+
+const app = createApp(App)
+
+const Api = () => {
+	console.log('request')
+	return 'api test'
+}
+  
+// 插件自动加载
+const modules = import.meta.globEager('./modules/*.ts')
+Object.values(modules).forEach(v => {
+	if (typeof v.default === 'function') {
+		v.default(app)
+	}
+})
+app.config.globalProperties.$api = Api
+app.mount('#app')
---
+++ src/pages/Index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { log } from 'console';
+import { getCurrentInstance } from 'vue';
+const {proxy} = getCurrentInstance()
+const { t, locale } = useI18n();
+const { isDark, toggleDark } = useDarks();
+
+onMounted(() => {
+    console.log(proxy.$api())
+}) 
+
+const toggleLocale = () => {
+  locale.value = locale.value === "zh-CN" ? "en" : "zh-CN";
+};
+
+
+</script>
+
+<template>
+  <div class="box">
+    <div>You can you up，no can no BB</div>
+    <div class="cursor-pointer m-6" @click="toggleDark()">
+      light: {{ isDark }} click me!!
+    </div>
+
+    <div class="cursor-pointer m-6" @click="toggleLocale()">
+      language: {{ t("index") }} click me!!
+    </div>
+  </div>
+</template>
